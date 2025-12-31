d90556f39b492f94d709785250e088ba4779dc02
Slava Pavlutin <sl.pavlutin@gmail.com>
2025-12-31 11:44:34 +0300

    refactoring

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -12,30 +12,31 @@ type config struct {
 	staticDir string
 }
 
+type application struct {
+	infoLogger  *log.Logger
+	errorLogger *log.Logger
+}
+
 func main() {
 	var cfg config
 	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
 	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "HTTP network address")
 	flag.Parse()
 
-	infoLogger := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-	errorLogger := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app := &application{
+		infoLogger:  log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime),
+		errorLogger: log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile),
+	}
 
-	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir(cfg.staticDir))
+	mux := app.routes()
 
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippets", snippetView)
-	mux.HandleFunc("/snippets/new", snippetCreate)
-
-	srv := http.Server{
+	srv := &http.Server{
 		Addr:     cfg.addr,
-		ErrorLog: errorLogger,
+		ErrorLog: app.errorLogger,
 		Handler:  mux,
 	}
 
-	infoLogger.Printf("starting server on %s", cfg.addr)
+	app.infoLogger.Printf("starting server on %s", cfg.addr)
 	err := srv.ListenAndServe()
-	errorLogger.Fatal(err)
+	app.errorLogger.Fatal(err)
 }
---
+++ cmd/web/routes.go
@@ -0,0 +1,13 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() *http.ServeMux {
+	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippets", app.snippetView)
+	mux.HandleFunc("/snippets/new", app.snippetCreate)
+	return mux
+}
