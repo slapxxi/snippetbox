a28c42c75d575dee46dcd16b1229aff28e9146dc
Slava Pavlutin <sl.pavlutin@gmail.com>
2026-01-02 13:08:27 +0300

    parse all partials

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -14,6 +14,7 @@ type templateData struct {
 
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
+
 	pages, err := filepath.Glob("./ui/html/pages/*.html")
 	if err != nil {
 		return nil, err
@@ -21,16 +22,22 @@ func newTemplateCache() (map[string]*template.Template, error) {
 
 	for _, page := range pages {
 		name := filepath.Base(page)
-		files := []string{
-			"./ui/html/index.html",
-			"./ui/html/partials/nav.html",
-			page,
-		}
 
-		templ, err := template.ParseFiles(files...)
+		templ, err := template.ParseFiles("./ui/html/index.html")
 		if err != nil {
 			return nil, err
 		}
+
+		templ, err = templ.ParseGlob("./ui/html/partials/*.html")
+		if err != nil {
+			return nil, err
+		}
+
+		templ, err = templ.ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+
 		cache[name] = templ
 	}
 
