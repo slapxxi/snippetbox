2d3f4a7fcb2e4d225ef2f16a8e0894ace195bd13
Slava Pavlutin <sl.pavlutin@gmail.com>
2026-01-02 14:04:04 +0300

    middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,13 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
 
-func (app *application) routes() *http.ServeMux {
+	"github.com/justinas/alice"
+)
+
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
+
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippets", app.snippetView)
 	mux.HandleFunc("/snippets/new", app.snippetCreate)
-	return mux
+
+	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
+	return standard.Then(mux)
 }
